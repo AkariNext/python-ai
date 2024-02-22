f6548bea19b5aaca8f08bafa445aa969d6ccc218
yupix <yupi0982@outlook.jp>
2024-02-22 21:32:42 +0900

    chore: Noteを保存しすぎないように調整

--- src/tui/store/timeline.py
+++ src/tui/store/timeline.py
@@ -3,4 +3,8 @@ from mipac.models.note import Note
 from src.utils.tui.storage import UseStorage
 
 
-TIMELINE = UseStorage[list[Note]](default=[])
+def vacuum_overflow(timeline: list[Note]) -> list[Note]:
+    return timeline[-100:]
+
+
+TIMELINE = UseStorage[list[Note]](default=[], callback=vacuum_overflow)
--- src/utils/tui/storage.py
+++ src/utils/tui/storage.py
@@ -1,13 +1,20 @@
+from typing import Callable
+
+
 class UseStorage[T]:
-    def __init__(self, default: T):
+    def __init__(self, default: T, *, callback: Callable | None = None):
         self.observers = []
         self._value: T = default
+        self.callback = callback
 
     def subscribe(self, func):
         self.observers.append(func)
 
     def set(self, value: T):
+        if self.callback:
+            value = self.callback(value)
         self._value = value
+
         for observer in self.observers:
             observer(value)
 
